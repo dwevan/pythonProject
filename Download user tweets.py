3f40b3d2b92cb908ef534478f440e573cccd23b7
Dwevan <Devan.williams@hotmail.com>
2022-01-03 18:52:44 +0000

    downloaded all tweets for past year

--- Download user tweets.py
+++ Download user tweets.py
@@ -1,14 +1,16 @@
+from time import time
 import tweepy
 import twitter_credentials as twcr
+import datetime
 
 # Authorize Twitter credentials
 auth = tweepy.OAuthHandler(twcr.consumer_key, twcr.consumer_secret)
 auth.set_access_token(twcr.access_token, twcr.access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit=True)
 
-#steps taken from (https://fairyonice.github.io/extract-someones-tweet-using-tweepy.html)
+#steps taken from (https://fairyonice.github.io/extract-someones-tweet-using-tweepy.html, and https://towardsdatascience.com/how-to-build-a-dataset-from-twitter-using-python-tweepy-861bdbc16fa5
 
-userID = "dwevan"
+userID = "@UKCP_Updates"
 
 tweets = api.user_timeline(screen_name=userID, 
                            # 200 is the maximum allowed count
@@ -19,13 +21,6 @@ tweets = api.user_timeline(screen_name=userID,
                            tweet_mode = 'extended'
                            )
 
-for info in tweets[:3]:
-    print("ID {}".format(info.id))
-    print(info.created_at)
-    print(info.full_text)
-    print("\n")
-    
-    
 all_tweets = []
 all_tweets.extend(tweets)
 oldest_id = tweets[-1].id
@@ -44,16 +39,17 @@ while True:
     oldest_id = tweets[-1].id
     all_tweets.extend(tweets)
     print('N of tweets downloaded till now {}'.format(len(all_tweets)))
-    
-tweet.full_text.encode("utf-8")
+
+#code to label file with datestamp of when data taken
+current_date_and_time = str(datetime.date.today())    
+filename =  userID + '_tweets_at_' + current_date_and_time
 
 from pandas import DataFrame
 outtweets = [[tweet.id_str, 
               tweet.created_at, 
-              tweet.favorite_count, 
-              tweet.retweet_count, 
               tweet.full_text.encode("utf-8").decode("utf-8")] 
              for idx,tweet in enumerate(all_tweets)]
-df = DataFrame(outtweets,columns=["id","created_at","favorite_count","retweet_count", "text"])
-df.to_csv('%s_tweets.csv' % userID,index=False)
-df.head(3)
+df = DataFrame(outtweets,columns=["id","created_at", "text"])
+df.to_csv(filename + '.csv' ,index=False)
+
+print (filename)
