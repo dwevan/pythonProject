a8986e2d9b96a48d9575070b6fe29c48328d5a4b
Dwevan <Devan.williams@hotmail.com>
2022-01-04 21:04:43 +0000

    .

--- scratch.py
+++ scratch.py
@@ -11,3 +11,6 @@ combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
 
 #export to csv
 combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+
+#https://www.freecodecamp.org/news/how-to-combine-multiple-csv-files-with-8-lines-of-code-265183e0854/
+#https://stackoverflow.com/questions/45211681/add-column-to-csv-file-and-insert-filename-in-that-column
