7855c50dd987074772258f522133f08fdb37083a
Dwevan <43811522+dwevan@users.noreply.github.com>
2022-03-13 21:05:05 +0000

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-# variables that contain the user credentials to access twitter API
-consumer_key = "LKsEKWAz20mGJNqKgvTlA7Gwa"
-consumer_secret = "uqWPHYuD1xZmdgPk1xhJo4vQ6kZaaTVAXcGF3wfxFmGj9t7yHK"
-access_token = "188856232-EcG1fWrdfYcojF553yA1yy5f2YxVZaXZBJA7Nk6P"
-access_token_secret = "Ig1PbyfQw3XfYvAuvndrv7fC4b0ppKzUKF4GKQ8hGu6DW"
